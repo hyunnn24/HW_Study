c87d3582701e7d6486cdecc96252d3ea5b216414
hyunnn24 <snow3515@naver.com>
2024-09-12 02:05:17 +0000

    Page function Test

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 import streamlit as st
 
-
-
-
+def Abstract():
+    st.write("this page works well")
+def Autodesek_Fusion():
+    st.write("this page works well")
+def PCB_Basic():
+    st.write("this page works well")
+def Projects():
+    st.write("this page works well")
+def Advanced():
+    st.write("this page works well")
 
 #페이지 
 page = st.sidebar.selectbox("HW STUDY", ["Abstract","Autodesek Fusion","PCB Basic","Projects (Open Source)", "Advanced (Optional)"])
 st.sidebar.text("  made by hyunnn :kr: \n  CERT-IS PKNU \n  2024.09.13")
 
 if page == "Abstract":
-    st.write("hello!")
+    Abstract()
 elif page == "Autodesek Fusion":
-    st.write("hello!")
+    Autodesek_Fusion()
 elif page == "PCB Basic":
-    st.write("hello!")
+    PCB_Basic()
 elif page == "Projects (Open Source)":
-    st.write("hello!")
+    Projects()
 elif page == "Advanced (Optional)":
-    st.write("hello!")
+    Advanced()
 #elif page == "":
 
 #elif page == "":
