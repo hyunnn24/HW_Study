9d007e8e2e7ac6b8fa15248cc575b1e7c21da609
hyunnn24 <snow3515@naver.com>
2024-09-12 01:38:29 +0000

    Page Create

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import streamlit as st
 
+
+
+
+
+#페이지 
+page = st.sidebar.selectbox("HW STUDY"), ["Abstract","Autodesek Fusion","PCB Basic","Projects (Open Source)", "Advanced (Optional)"])
+st.sidebar.text("  made by hyunnn :kr: \n  CERT-IS PKNU \n  2024.09.13")
+
+if page == "Abstract":
+    st.write("hello!")
+elif page == "Autodesek Fusion":
+    st.write("hello!")
+elif page == "PCB Basic":
+    st.write("hello!")
+elif page == "Projects (Open Source)":
+    st.write("hello!")
+elif page == "Advanced (Optional)":
+    st.write("hello!")
+#elif page == "":
+
+#elif page == "":
